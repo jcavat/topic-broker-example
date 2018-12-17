08061fb93d1dec8e2e2cfe48c4c2859250b4125c
Joel Cavat <jcavat@gmail.com>
2018-12-17 16:31:54 +0100

    Change Server name to Broker

---
+++ src/main/java/ch/hepia/communication/Broker.java
@@ -0,0 +1,34 @@
+package ch.hepia.communication;
+
+import javax.jms.Connection;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+
+public class Broker {
+
+    public Broker(String ipBroker) {
+        try {
+            final String brokerUrl = "tcp://" + ipBroker + ":61616";
+            final BrokerService broker = new BrokerService();
+
+            broker.setPersistent(false);
+            broker.setUseJmx(false);
+            broker.addConnector(brokerUrl);
+            broker.start();
+
+            final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+            final Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+
+        new Broker( args.length == 1 ? args[0] : "localhost");
+    }
+}
